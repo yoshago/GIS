7e492051a7da14dec9a6652516fc59816b85bf68
ישי <@yishay>
2017-11-22 16:38:06 +0200

    added class readTst

---
+++ GIS/src/readTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author ישי
+ *
+ */
+public class ReadTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
