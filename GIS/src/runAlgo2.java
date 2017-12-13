b9a9fc672725ed655e1092c2816cce8cadc990aa
ישי <@yishay>
2017-12-13 17:37:12 +0200

    run changed

--- GIS/src/runAlgo2.java
+++ GIS/src/runAlgo2.java
@@ -1,20 +1,17 @@
-import java.util.Scanner;
+/**
+ * 
+ */
 
 /**
  * @author ישי
  *
  */
-public class Run {
+public class runAlgo2 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-//		System.out.println("please enter path for intput");
-//		Scanner userInput=new Scanner(System.in);
-//		String inputPath=userInput.nextLine();
-//		Folder f=new Folder(inputPath);
-//		processCsv processCsv=new processCsv(f.getOutputPath());
 		LocationFinder lf=new LocationFinder("C:\\Users\\ישי\\Desktop\\testalgo","C:\\Users\\ישי\\Desktop\\testnogps");
 		lf.findLocation();
 		write.writeCsvFile(lf.input, "C:\\Users\\ישי\\Desktop\\testnogps\\check1.csv");
